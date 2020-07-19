1469b4351ed7d1e18cf8c93d38a5542837a670f1
Daniel Parra <danialepaco@hotmail.com>
2020-07-18 21:51:18 -0500

    8

--- Pokedex/AppDelegate.swift
+++ Pokedex/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application 
+        // Override point for customization after applicatio
         return true
     }
 
